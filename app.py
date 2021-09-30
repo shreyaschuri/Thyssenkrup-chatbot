21511d989f512f812c779ce71fd30d8166989065
Kira00769 <55293445+shreyaschuri@users.noreply.github.com>
2021-09-30 18:29:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+
+app = Flask(__name__)
+
+bot = ChatBot("Candice",  storage_adapter="chatterbot.storage.MongoDatabaseAdapter", logic_adapters=[
+    {
+        'import_path': 'chatterbot.logic.BestMatch',
+        'default_response': 'I am sorry, but I do not understand.',
+        'maximum_similarity_threshold': 1.0
+    }
+]
+)
+trainer = ListTrainer(bot)
+trainer = ChatterBotCorpusTrainer(bot)
+
+#trainer.train('../test.json')
+trainer.train('./conv.yml')
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get")
+def get_bot_response():
+     userText = request.args.get('msg')
+     return str(bot.get_response(userText))
+
+
+    # print(request.args.get('msg'))
+
+
+
+if __name__ == "__main__":
+    app.run()
